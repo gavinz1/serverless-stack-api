4b613e1675e3c50c53245d97369031553a997d0a
Zhaaaaaaang <gavinzhang111@gmail.com>
2021-01-06 07:40:51 -0800

    adding our Serverless API

---
+++ libs/handler-lib.js
@@ -0,0 +1,21 @@
+export default function handler(lambda) {
+    return function (event, context) {
+        return Promise.resolve()
+        .then(() => lambda(event, context))
+        //on success
+        .then((responseBody) => [200, responseBody])
+        //on failure
+        .catch((e) => {
+            return [500, {error: e.message}];
+        })
+
+        .then(([statusCode, body]) => ({
+            statusCode,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Credentials": true,
+            },
+            body: JSON.stringify(body),
+        }));
+    };
+}
